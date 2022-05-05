f8682b76297185e46c8425cda63cf5f7f6a645f4
Hillary Smith <hillaryhsmith@gmail.com>
2022-05-05 11:11:21 -0700

    set up for testing completed

--- app/__init__.py
+++ app/__init__.py
@@ -1,21 +1,29 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from dotenv import load_dotenv
+import os
 
 db = SQLAlchemy()
 migrate = Migrate()
+load_dotenv
 
 def create_app(test_config=None):
     app = Flask(__name__)
 
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/solar_system_development'
-
+    if not test_config:
+        app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+        app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("SQLALCHEMY_DATABASE_URI")
+    else: 
+        app.config["TESTING"] = True
+        app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+        app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("SQLALCHEMY_TEST_DATABASE_URI")
+    
     db.init_app(app)
     migrate.init_app(app, db)
 
     # Register Blueprints 
-    from .routes import planet_routes
+    from .routes import planet_routes #is this better or worse than following the way in Learn which separates Planet model import and bp
     app.register_blueprint(planet_routes.bp)
 
     return app
